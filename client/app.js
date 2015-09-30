e33ccb5e08564900d86403ac3b2ec10b306f63ef
Renee Yonamine <reneey@hawaii.edu>
2015-09-30 10:06:44 -1000

    Added Week 6 in-class Assignment

---
+++ client/app.js
@@ -0,0 +1,17 @@
+// adds index to each item
+UI.registerHelper('indexedArray', function(context, options) {
+  if (context) {
+    return context.map(function(item, index) {
+      item._index = index;
+      return item;
+    });
+  }
+});
+
+Template.body.helpers({
+
+  polls: function() {
+    return Polls.find();
+  }
+
+});
---
+++ collections/polls.js
@@ -0,0 +1,5 @@
+/**
+ * Created by Renee on 9/30/2015.
+ */
+
+Polls = new Mongo.Collection('polls');
